58dc6fcd3383fc35b5a0a88e5534e7cc5a75b011
Aman <amancloud0201@gmail.com>
2025-05-25 03:25:05 +0530

    first commit

---
+++ src/components/sections/Skills.jsx
@@ -0,0 +1,157 @@
+import React from "react";
+import styled from "styled-components";
+import { skills } from "../../data/constants";
+import { Tilt } from "react-tilt";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-contnet: center;
+  position: rlative;
+  z-index: 1;
+  align-items: center;
+`;
+
+const Wrapper = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+  max-width: 1100px;
+  gap: 12px;
+  @media (max-width: 960px) {
+    flex-direction: column;
+  }
+`;
+const Title = styled.div`
+  font-size: 52px;
+  text-align: center;
+  font-weight: 600;
+  margin-top: 20px;
+  color: ${({ theme }) => theme.text_primary};
+  @media (max-width: 768px) {
+    margin-top: 12px;
+    font-size: 32px;
+  }
+`;
+const Desc = styled.div`
+  font-size: 18px;
+  text-align: center;
+  font-weight: 600;
+  color: ${({ theme }) => theme.text_secondary};
+  @media (max-width: 768px) {
+    font-size: 16px;
+  }
+`;
+
+const SkillsContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 20px;
+  gap: 50px;
+  justify-content: center;
+`;
+
+const Skill = styled.div`
+  width: 100%;
+  max-width: 500px;
+  background-color: rgba(17, 25, 40, 0.83);
+  border: 1px solid rgba(255, 255, 255, 0.125);
+  box-shadow: rgba(23, 92, 230, 0.15) 0px 4px 24px;
+  border-radius: 16px;
+  padding: 18px 36px;
+  @media (max-width: 768px) {
+    max-width: 400px;
+    padding: 10px 36px;
+  }
+
+  @media (max-width: 500px) {
+    max-width: 330px;
+    padding: 10px 36px;
+  }
+`;
+
+const SkillTitle = styled.div`
+  font-size: 28px;
+  font-weight: 600;
+  margin-bottom: 20px;
+  text-align: center;
+  color: ${({ theme }) => theme.text_secondary};
+`;
+
+const SkillList = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 12px;
+  margin-bottom: 20px;
+`;
+const SkillItem = styled.div`
+  font-size: 16px;
+  font-weight: 400;
+  color: ${({ theme }) => theme.text_primary + 80};
+  border: 1px solid ${({ theme }) => theme.text_primary + 80};
+  border-radius: 12px;
+  padding: 12px 16px;
+  display: flex;
+  align-items: center;
+  gap: 16px;
+  min-width: 250px;
+
+  @media (max-width: 768px) {
+    font-size: 14px;
+    padding: 10px 12px;
+  }
+`;
+
+const SkillImage = styled.img`
+  width: 60px;
+  height: 60px;
+  border-radius: 50%; /* Makes the image circular */
+  object-fit: cover;
+`;
+
+
+const Skills = () => {
+  return (
+    <Container id="Skills">
+      <Wrapper>
+        <Title>Team Details</Title>
+        <Desc
+          style={{
+            marginBottom: "40px",
+          }}
+        >
+          Here are all the listed Team Members:
+        </Desc>
+
+        <SkillsContainer>
+          {skills.map((member, index) => (
+            <Tilt key={`member-${index}`}>
+              <Skill>
+                <SkillTitle>{member.title}</SkillTitle>
+                <SkillList>
+                  <SkillItem>
+                    <SkillImage src={member.image} alt={member.title} />
+                    <div>
+                      <div>
+                        <strong>{member.designation}</strong>
+                      </div>
+                      <div>{member.branch}</div>
+                      <div>{member.year}</div>
+                    </div>
+                  </SkillItem>
+                </SkillList>
+              </Skill>
+            </Tilt>
+          ))}
+        </SkillsContainer>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Skills;
